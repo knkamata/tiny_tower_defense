915f927b5b46d78827fc6c82b58ee4baeb02b0e1
Ken Kamata <kn12kamata@gmail.com>
2021-12-22 12:14:14 +0900

    Change window name to 'Tiny Tower Defense'

--- Game.cpp
+++ Game.cpp
@@ -23,7 +23,7 @@ bool Game::Initialize()
 		return (false);
 	}
 
-	mWindow = SDL_CreateWindow("Game Prog C02", 100, 100, 1024, 768, 0);
+	mWindow = SDL_CreateWindow("Tiny Tower Defense", 100, 100, 1024, 768, 0);
 	if (!mWindow)
 	{
 		SDL_Log("Failed to create window: %s", SDL_GetError());
